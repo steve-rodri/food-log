7d81b7344d61a1a3d7eb24a655315a3b0912a8e3
Steve <steve.rodri@gmail.com>
2018-12-31 02:13:16 -0500

    update

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     super(props)
     this.state = {
       userName: "",
-      currentView: "Add Food",
+      currentView: "Login",
       prevView: '',
       log: {
         meals: [],
